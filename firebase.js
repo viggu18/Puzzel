77b00a6094e809651e95bb526eb4f6b339a319f4
Vignesh <nayakvignesh18@gmail.com>
2023-08-24 12:07:52 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,6 @@
 import * as firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAfhATjSu0T7Aip51NIujkPpWH-HvskpS0",
-  authDomain: "puzzel-12430.firebaseapp.com",
-  projectId: "puzzel-12430",
-  storageBucket: "puzzel-12430.appspot.com",
-  messagingSenderId: "913757833278",
-  appId: "1:913757833278:web:bfa1ba31df369f0dfea96f",
-  measurementId: "G-E8LVMNE6LG"
-  };
+//firebaseConfig here... 
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
